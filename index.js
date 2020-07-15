2c36065ca9b1ae7a03cd5577e25c888387ece932
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-07-15 10:05:55 -0400

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,97 @@ let allWagesFor = function () {
 
     return payable
 }
+
+const createEmployeeRecord = (arr) => {
+    let employee = {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+    return employee;
+};
+
+const createEmployeeRecords = (arrOfarrs) => {
+    return arrOfarrs.map(createRecord);
+};
+
+const createTimeInEvent = (dateStamp) => {
+    let type = "TimeIn";
+    let hour = dateStamp;
+    let date = dateStamp.split(" ")[0];
+    let punchIn = {
+        type,
+        hour,
+        date
+    };
+
+    return createEmployeeRecord.call(this, dateStamp)
+};
+
+// TESTING AREA 
+
+let bpRecord, updatedBpRecord, newEvent
+bpRecord = createEmployeeRecord(["Byron", "Poodle", "Mascot", 3])
+updatedBpRecord = createTimeInEvent.call(bpRecord, "2014-02-28 1400")
+newEvent = updatedBpRecord.timeInEvents[0]
+
+console.log(updatedBpRecord)
+
+// ===================================
+
+const createTimeOutEvent = (empRec, dateStamp) => {
+    let type = "TimeOut";
+    let hour = parseInt(dateStamp.split(" ")[1]);
+    let date = dateStamp.split(" ")[0];
+    let punchOut = {
+        type,
+        hour,
+        date,
+    };
+    empRec.timeOutEvents.push(punchOut);
+    return empRec;
+};
+
+const hoursWorkedOnDate = (empRec, date) => {
+    let timeInhour = empRec.timeInEvents.filter(getDate)[0].hour;
+    let timeOutHour = empRec.timeOutEvents.filter(getDate)[0].hour;
+    let total;
+
+    total = (timeOutHour - timeInhour) / 100;
+    return total;
+
+    function getDate(event) {
+        if (event.date === date) {
+            return event.date;
+        }
+    }
+};
+
+const wagesEarnedOnDate = (empRec, date) => {
+    return hoursWorkedOnDate(empRec, date) * empRec.payPerHour;
+};
+
+const findEmployeeByFirstName = (srcArray, firstName) => {
+    return srcArray.find(emplObj => emplObj.firstName === firstName)
+};
+
+const calculatePayroll = (arrOfEmpRecs) => {
+    return arrOfEmpRecs.map(getAllWages).reduce(getTotalWage)
+};
+
+// ITERATION HELPER METHODS IF CAN BE USED BY OTHER FUNCTIONS
+
+function getTotalWage(wage, total = 0) {
+    return wage + total;
+} // used by > func calculatePayroll() < and > func allWagesFor() <
+
+function getAllWages(emp) {
+    return allWagesFor(emp)
+} // used by: calculatePayroll()
+
+function createRecord(arr) {
+    return createEmployeeRecord(arr);
+} // used by: createEmployeeRecords()
